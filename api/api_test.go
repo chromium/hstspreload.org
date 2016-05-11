48cd473c79811d04012b188978b9f007f0830d88
Lucas Garron <lgarron@chromium.org>
2016-05-11 16:20:43 -0700

    Implement a test for api.Status().

--- api/api_test.go
+++ api/api_test.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/chromium/hstspreload.appspot.com/database"
@@ -15,6 +19,36 @@ func TestCheckConnection(t *testing.T) {
 
 	ms.FailCalls = true
 	if err := a.CheckConnection(); err == nil {
-		t.Errorf("connection should fail")
+		t.Error("connection should fail")
 	}
+}
+
+func TestStatus(t *testing.T) {
+	ms := database.MockState{}
+	a := API{database.Mock{State: &ms}}
+
+	w := httptest.NewRecorder()
+
+	r, err := http.NewRequest("GET", "?domain=garron.net", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b := &bytes.Buffer{}
+	w.Body = b
+
+	a.Status(w, r)
+
+	s := database.DomainState{}
+	if err := json.Unmarshal(w.Body.Bytes(), &s); err != nil {
+		t.Fatal(err)
+	}
+
+	if s.Name != "garron.net" {
+		t.Errorf("Wrong name: %s", s.Name)
+	}
+	if s.Status != database.StatusUnknown {
+		t.Errorf("Wrong status: %s", s.Status)
+	}
+
 }
