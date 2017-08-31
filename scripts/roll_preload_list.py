0f9f51601095927c22905da780d855695989c3dd
Lucas Garron <lgarron@chromium.org>
2017-08-30 17:36:51 -0700

    Update preload list roller.

--- scripts/roll_preload_list.py
+++ scripts/roll_preload_list.py
@@ -1,3 +1,4 @@
+import argparse
 import base64
 import json
 import re
@@ -14,14 +15,31 @@ def getPendingRemovals():
   log("Fetching pending removal...\n")
   return requests.get("https://hstspreload.org/api/v2/pending-removal").json()
 
-def getRawText():
+def getRawText(preload_list_path):
   log("Fetching preload list from Chromium source...\n")
-  with open("/Users/lgarron/alt/src/net/http/transport_security_state_static.json", "r") as f:
+  with open(preload_list_path, "r") as f:
       s = f.read()
   return s
 
+def getPendingScan(pendingDataFilePath):
+  log("Fetching pending list from provided path...\n")
+  with open(pendingDataFilePath, "r") as f:
+      return json.load(f)
+
+def domainsToPreload(pendingData):
+  numSkipping = 0
+  numPreloading = 0
+  for result in pendingData:
+    if len(result["issues"]["errors"]) == 0:
+      numPreloading += 1
+      yield result["domain"]
+    else:
+      numSkipping += 1
+  log("Pending entries preloaded: %d\n" % numPreloading)
+  log("Pending entries skipped: %d\n" % numSkipping)
+
 def chunks(rawText):
-  log("Iterating...\n")
+  log("Chunking...\n")
   lines = iter(rawText.splitlines())
   while True:
     try:
@@ -39,7 +57,8 @@ def chunks(rawText):
     except StopIteration:
       break
 
-def removePendingRemovals(pendingRemovals, entryStrings):
+def update(pendingRemovals, pendingAdditions, entryStrings):
+  log("Updating...\n")
   removedCount = 0
   for l, c in entryStrings:
     if c == Chunk.OneLineEntry:
@@ -50,22 +69,37 @@ def removePendingRemovals(pendingRemovals, entryStrings):
         pendingRemovals.remove(domain)
       else:
         yield l
+    elif l == "    // END OF BULK ENTRIES":
+      for domain in pendingAdditions:
+        yield '    { "name": "%s", "include_subdomains": true, "mode": "force-https" },' % domain
+      yield l
     else:
       yield l
-  log("Removed: %s" % removedCount)
+  log("Removed: %s\n" % removedCount)
 
-# def write(bulkDomains):
-#   log("Writing...\n")
-#   with open(sys.argv[1], 'w') as file:
-#     json.dump(formatForGo(bulkDomains), file)
+def write(preload_list_path, output):
+  log("Overwriting preload list source...\n")
+  with open(preload_list_path, 'w') as file:
+    file.write(output)
+
+def getArgs():
+  parser = argparse.ArgumentParser(description='Roll the HSTS preload list (experimental).')
+  parser.add_argument('preload_list_path', type=str)
+  parser.add_argument('pending_scan_path', type=str)
+  return parser.parse_args()
 
 def main():
-  rawText = getRawText()
+  args = getArgs()
+
+  rawText = getRawText(args.preload_list_path)
   pendingRemovals = getPendingRemovals()
-  removalsDone = removePendingRemovals(pendingRemovals, chunks(rawText))
+  pendingAdditions = domainsToPreload(getPendingScan(args.pending_scan_path))
+  removalsDone = update(pendingRemovals, pendingAdditions, chunks(rawText))
+
+  output = ""
   for l in removalsDone:
-    print l
-  log(str(pendingRemovals))
+    output += l + "\n"
+  write(args.preload_list_path, output)
 
 if __name__ == "__main__":
     main()
