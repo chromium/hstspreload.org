1e04863a3b31e60d63d44edd73844d1786c8d46c
Martijn Croonen <martijn@martijnc.be>
2017-11-07 19:15:07 +0100

    Re-order fields in the auto-roller script.

--- scripts/roll_preload_list.py
+++ scripts/roll_preload_list.py
@@ -76,7 +76,7 @@ def update(pendingRemovals, pendingAdditions, entryStrings):
         yield l
     elif l == "    // END OF 1-YEAR BULK HSTS ENTRIES":
       for domain in sorted(pendingAdditions):
-        yield '    { "name": "%s", "policy": "bulk-1-year", "include_subdomains": true, "mode": "force-https" },' % domain
+        yield '    { "name": "%s", "policy": "bulk-1-year", "mode": "force-https", "include_subdomains": true },' % domain
       yield l
     else:
       yield l
