4fe2449e204d93df06b06ce6e9341950330d5d29
Khushi Bhatamrekar <khushib@google.com>
2023-06-14 20:58:54 +0000

    datastore interface for automated removals

--- database/database_test.go
+++ database/database_test.go
@@ -234,3 +234,220 @@ func TestStatesWithStatus(t *testing.T) {
 		}
 	}
 }
+
+var setTests = []struct {
+	description       string
+	domainStates      []InvalidDomainState
+	wantStatusReports []string
+	wantStates        []InvalidDomainState
+}{
+	{
+		"two domains",
+		[]InvalidDomainState{
+			{Name: "youtube.com", Policy: "bulk-1-year"},
+			{Name: "garron.net", Policy: "bulk-1-year"},
+		},
+		[]string{"Updating 2 entries...", " done.\n"},
+		[]InvalidDomainState{
+			{Name: "youtube.com", Policy: "bulk-18-week"},
+			{Name: "garron.net", Policy: "bulk-1-year"},
+		},
+	},
+	{
+		"bulk-18-week",
+		[]InvalidDomainState{{Name: "gmail.com", Policy: "bulk-18-week"}},
+		[]string{"Updating 1 entries...", " done.\n"},
+		[]InvalidDomainState{{Name: "gmail.com", Policy: "bulk-18-week"},
+		{Name: "youtube.com", Policy: "bulk-18-week"},
+		{Name: "garron.net", Policy: "bulk-1-year"},},
+	},
+	{
+		"bulk-1-year",
+		[]InvalidDomainState{{Name: "wikipedia.com", Policy: "bulk-1-year"}},
+		[]string{"Updating 1 entries...", " done.\n"},
+		[]InvalidDomainState{{Name: "wikipedia.com", Policy: "bulk-1-year"},
+		{Name: "gmail.com", Policy: "bulk-18-week"},
+		{Name: "youtube.com", Policy: "bulk-18-week"},
+		{Name: "garron.net", Policy: "bulk-1-year"},},
+	},
+}
+
+// Test PutStates and AllDomainStates.
+func TestSet(t *testing.T) {
+	resetDB()
+
+	for _, tt := range setTests {
+
+		var statuses []string
+		statusReport := func(format string, args ...interface{}) {
+			formatted := fmt.Sprintf(format, args...)
+			statuses = append(statuses, formatted)
+		}
+
+		err := testDB.SetInvalidDomains(
+			tt.domainStates,
+			statusReport,
+		)
+		if err != nil {
+			t.Errorf("[%s] cannot put states %s", tt.description, err)
+			return
+		}
+
+		if !reflect.DeepEqual(statuses, tt.wantStatusReports) {
+			t.Errorf("[%s] Incorrect status reports: %#v", tt.description, statuses)
+		}
+
+		for p := range statuses {
+			println(p)
+		}
+	}
+}
+
+// Test Set and Get
+func TestSetAndGet(t *testing.T) {
+	domainA := InvalidDomainState{Name: "a.com", Policy: "bulk-1-year"}
+	domainB := InvalidDomainState{Name: "b.com", Policy: "bulk-18-weeks"}
+	domainC := InvalidDomainState{Name: "c.com", Policy: "bulk-18-weeks"}
+	domainD := InvalidDomainState{Name: "d.com", Policy: "bulk-1-year"}
+	domainE := InvalidDomainState{Name: "e.com", Policy: "bulk-1-year"}
+	domainG := InvalidDomainState{Name: "g.com", Policy: "bulk-18-weeks"}
+	domainH := InvalidDomainState{Name: "h.com", Policy: "bulk-18-weeks"}
+	domainI := InvalidDomainState{Name: "i.com", Policy: "bulk-1-year"}
+	domainJ := InvalidDomainState{Name: "j.com", Policy: "bulk-18-weeks"}
+	domainK := InvalidDomainState{Name: "k.com", Policy: "bulk-1-year"}
+	resetDB()
+
+	domainStates, err := testDB.GetInvalidDomains([]string{"a.com"})
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	// domainStates should be empty as domains are not added to database
+	if (len(domainStates) != 0) {
+		t.Errorf("Empty database should contain no preloaded domains")
+	}
+
+	testing := []string{domainA.Name, domainB.Name, domainC.Name, domainD.Name, domainE.Name, domainG.Name, domainH.Name, domainI.Name, domainJ.Name, domainK.Name}
+	table := []struct {
+		name    string
+		domains []InvalidDomainState // sorted order
+	}{
+		{"a.com", []InvalidDomainState{domainA}},
+		{"b.com", []InvalidDomainState{domainB}},
+		{"c.com", []InvalidDomainState{domainC}},
+		{"d.com", []InvalidDomainState{domainD}},
+		{"e.com", []InvalidDomainState{domainE}},
+		{"g.com", []InvalidDomainState{domainG}},
+		{"h.com", []InvalidDomainState{domainH}},
+		{"i.com", []InvalidDomainState{domainI}},
+		{"j.com", []InvalidDomainState{domainJ}},
+		{"k.com", []InvalidDomainState{domainK}},
+	}
+
+	var statuses []string
+	statusReport := func(format string, args ...interface{}) {
+		formatted := fmt.Sprintf(format, args...)
+		statuses = append(statuses, formatted)
+	}
+
+	for _, tt := range table {
+
+		err := testDB.SetInvalidDomains(
+			tt.domains,
+			statusReport,
+		)
+		if err != nil {
+			t.Errorf("[%s] cannot put states %s", tt.name, err)
+			return
+		}
+	}
+
+	domainStates, err = testDB.GetInvalidDomains(testing)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
+	for i := 0; i < len(domainStates); i++ {
+		if domainStates[i].Name != testing[i]{
+			t.Errorf("unexpected domain for domain name %s with:"+domainStates[i].Name, testing[i])
+		}
+	}
+}
+
+func TestSetGetAndDelete(t *testing.T) {
+	domainA := InvalidDomainState{Name: "a.com", Policy: "bulk-1-year"}
+	domainB := InvalidDomainState{Name: "b.com", Policy: "bulk-18-weeks"}
+	domainC := InvalidDomainState{Name: "c.com", Policy: "bulk-18-weeks"}
+	domainD := InvalidDomainState{Name: "d.com", Policy: "bulk-1-year"}
+	domainE := InvalidDomainState{Name: "e.com", Policy: "bulk-1-year"}
+	domainG := InvalidDomainState{Name: "g.com", Policy: "bulk-18-weeks"}
+	domainH := InvalidDomainState{Name: "h.com", Policy: "bulk-18-weeks"}
+	domainI := InvalidDomainState{Name: "i.com", Policy: "bulk-1-year"}
+	domainJ := InvalidDomainState{Name: "j.com", Policy: "bulk-18-weeks"}
+	domainK := InvalidDomainState{Name: "k.com", Policy: "bulk-1-year"}
+	resetDB()
+
+	domainStates, err := testDB.GetInvalidDomains([]string{"a.com"})
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	// domainStates should be empty as domains are not added to database
+	if (len(domainStates) != 0) {
+		t.Errorf("Empty database should contain no preloaded domains")
+	}
+
+	testing := []string{domainA.Name, domainB.Name, domainC.Name, domainD.Name, domainE.Name, domainG.Name, domainH.Name, domainI.Name, domainJ.Name, domainK.Name}
+	table := []struct {
+		name    string
+		domains []InvalidDomainState // sorted order
+	}{
+		{"a.com", []InvalidDomainState{domainA}},
+		{"b.com", []InvalidDomainState{domainB}},
+		{"c.com", []InvalidDomainState{domainC}},
+		{"d.com", []InvalidDomainState{domainD}},
+		{"e.com", []InvalidDomainState{domainE}},
+		{"g.com", []InvalidDomainState{domainG}},
+		{"h.com", []InvalidDomainState{domainH}},
+		{"i.com", []InvalidDomainState{domainI}},
+		{"j.com", []InvalidDomainState{domainJ}},
+		{"k.com", []InvalidDomainState{domainK}},
+	}
+
+	var statuses []string
+	statusReport := func(format string, args ...interface{}) {
+		formatted := fmt.Sprintf(format, args...)
+		statuses = append(statuses, formatted)
+	}
+
+	for _, tt := range table {
+
+		err := testDB.SetInvalidDomains(
+			tt.domains,
+			statusReport,
+		)
+		if err != nil {
+			t.Errorf("[%s] cannot put states %s", tt.name, err)
+			return
+		}
+	}
+
+	err = testDB.DeleteInvalidDomains(testing)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
+	for i := 0; i < len(domainStates); i++ {
+		if domainStates[i].Name != testing[i]{
+			t.Errorf("unexpected domain for domain name %s with:"+domainStates[i].Name, testing[i])
+		}
+	}
+
+	domainStates, err = testDB.GetInvalidDomains(testing)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
+	if (len(domainStates) != 0) {
+		t.Errorf("Empty database should contain no preloaded domains")
+	}
+
+}
