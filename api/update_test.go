4bcda2ed85f6101d576f387df760a6472d532965
Tami Kabiawu <kabiawu@google.com>
2023-06-23 22:37:18 +0000

    Made some changes to try and clean up the test fil

--- api/update_test.go
+++ api/update_test.go
@@ -8,13 +8,12 @@ import (
 	"time"
 
 	"github.com/chromium/hstspreload"
-	"github.com/chromium/hstspreload.org/database"
 	"github.com/chromium/hstspreload/chromium/preloadlist"
 )
 
 const ()
 
-// Testing that PolicyType is populated within the database when the Update Endpoint is called
+// TestPolicyType tests that PolicyType is populated within the database when the Update endpoint is called.
 func TestPolicyType(t *testing.T) {
 	api, mc, h, c := mockAPI(0 * time.Second)
 
@@ -37,24 +36,15 @@ func TestPolicyType(t *testing.T) {
 		"removal-pending-ineligible.test":          issuesWithErrors,
 	}
 
-	// Policy types set to preloadlist.Test for testing simplicity
 	pl1 := preloadlist.PreloadList{Entries: []preloadlist.Entry{
-		{Name: "garron.net", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: true, Policy: preloadlist.Test},
-		{Name: "chromium.org", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: false, Policy: preloadlist.Test},
-		{Name: "removal-preloaded-bulk-eligible.test", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: true, Policy: preloadlist.Test},
-		{Name: "removal-preloaded-not-bulk-eligible.test", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: true, Policy: preloadlist.Test},
+		{Name: "garron.net", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: true, Policy: preloadlist.Bulk18Weeks},
+		{Name: "chromium.org", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: false, Policy: preloadlist.Custom},
+		{Name: "removal-preloaded-bulk-eligible.test", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: true, Policy: preloadlist.UnspecifiedPolicyType},
+		{Name: "removal-preloaded-not-bulk-eligible.test", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: true, Policy: preloadlist.Bulk1Year},
 		{Name: "removal-preloaded-bulk-ineligible.test", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: true, Policy: preloadlist.Test},
-		{Name: "godoc.og", Mode: "", IncludeSubDomains: true, Policy: preloadlist.Test},
-		{Name: "dev", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: true, Policy: preloadlist.Test},
+		{Name: "godoc.og", Mode: "", IncludeSubDomains: true, Policy: preloadlist.Custom},
+		{Name: "dev", Mode: preloadlist.ForceHTTPS, IncludeSubDomains: true, Policy: preloadlist.UnspecifiedPolicyType},
 	}}
-
-	data1 := MockData{pr1, rr1, pl1}
-
-	jsonContentType := "application/json; charset=utf-8"
-
-	updateTest := apiTestCase{"garron.net pending", data1, failNone, api.Status, "GET", "?domain=garron.net",
-		200, jsonContentType, wantBody{state: &database.DomainState{
-			Name: "garron.net", Status: database.StatusPending}}}
 
 	h.preloadableResponses = pr1
 	h.removableResponses = rr1
@@ -66,9 +56,9 @@ func TestPolicyType(t *testing.T) {
 	w := httptest.NewRecorder()
 	w.Body = &bytes.Buffer{}
 
-	r, err := http.NewRequest(updateTest.method, updateTest.url, nil)
+	r, err := http.NewRequest("GET", "", nil)
 	if err != nil {
-		t.Fatalf("[%s] %s", updateTest.description, err)
+		t.Fatalf("[%s] %s", "NewRequest Failed", err)
 	}
 
 	api.Update(w, r)
@@ -77,10 +67,20 @@ func TestPolicyType(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Couldn't get the states of all domains in the database.")
 	}
+
+	expectedPolicies := map[string]preloadlist.PolicyType{
+		"garron.net":                               preloadlist.Bulk18Weeks,
+		"chromium.org":                             preloadlist.Custom,
+		"removal-preloaded-bulk-eligible.test":     preloadlist.UnspecifiedPolicyType,
+		"removal-preloaded-not-bulk-eligible.test": preloadlist.Bulk1Year,
+		"removal-preloaded-bulk-ineligible.test":   preloadlist.Test,
+		"godoc.og":                                 preloadlist.Custom,
+		"dev":                                      preloadlist.UnspecifiedPolicyType,
+	}
+
 	for _, state := range states {
-		// change to preloadlist.UnspecifiedPolicyType in due time
-		if state.Policy != preloadlist.Test {
-			t.Errorf("Policy field not accurately populated in the database for %s.", state.Name)
+		if state.Policy != expectedPolicies[state.Name] {
+			t.Errorf("Policy field not accurately populated in the database for %s with %s", state.Policy, expectedPolicies[state.Name])
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -4,14 +4,14 @@ go 1.15
 
 require (
 	cloud.google.com/go/datastore v1.11.0
-	github.com/chromium/hstspreload v0.0.0-20230620174236-bb638f02e21f
+	github.com/chromium/hstspreload v0.0.0-20230623210540-9e257cb2df03
 	golang.org/x/net v0.11.0
 	google.golang.org/api v0.128.0
 	google.golang.org/grpc v1.56.1
 )
 
 require (
-	cloud.google.com/go v0.110.2 // indirect
+	cloud.google.com/go v0.110.3 // indirect
 	cloud.google.com/go/compute v1.20.1 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.5 // indirect
 	github.com/googleapis/gax-go/v2 v2.11.0 // indirect
